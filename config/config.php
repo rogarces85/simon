e6e7dffee4a051f50b712a15d3d964c6c36b64b5
Rodrigo Garces <rodrigo.garces.guerrero@gmail.com>
2026-02-02 15:39:15 -0300

    Allow config.php to be pushed for deployment

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+// Configuration for Hostinger MySQL Database
+define('DB_HOST', 'srv1663.hstgr.io');
+define('DB_NAME', 'u901416689_runcoach');
+define('DB_USER', 'u901416689_runcoach');
+define('DB_PASS', '9D8Q2>uR');
+define('DB_CHARSET', 'utf8mb4');
+
+// Other configurations
+define('SITE_NAME', 'TrainPro');
+define('BASE_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/SIMON/'); // Adjust if needed
